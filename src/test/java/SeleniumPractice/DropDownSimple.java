e6f26de2c9a3de37a6039cceb5dada7ef041978f
Nhu Nguyen <nhu116677@gmail.comgit config --global user.name Nh116677git config --global user.email nhu116677@gmail.com>
2025-06-29 11:37:22 -0400

    Done

--- src/test/java/SeleniumPractice/DropDownSimple.java
+++ src/test/java/SeleniumPractice/DropDownSimple.java
@@ -1,7 +1,11 @@
 package SeleniumPractice;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class DropDownSimple {
     public static void main(String[] args) {
@@ -15,6 +19,18 @@ public class DropDownSimple {
         driver.get("https://syntaxprojects.com/no-select-tag-dropdown-demo.php#");
 
         //click on the dropdown to display the options
+
+        WebElement DD = driver.findElement(By.xpath("//div[@data-toggle=\"dropdown\"]"));
+        DD.click();
+
+        //Get all the options
+        List<WebElement> Options = driver.findElements(By.xpath("//ul[@class='dropdown-menu']/li/a"));
+        for (WebElement option : Options){
+            String optionText = option.getText();
+            if(optionText.equals("Kenya")){
+                option.click();
+            }
+        }
 
 
     }
