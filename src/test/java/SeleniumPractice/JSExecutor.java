8e18ec6d8cbbded25453c5dfac025ccc1e6b3d8e
Nhu Nguyen <nhu116677@gmail.comgit config --global user.name Nh116677git config --global user.email nhu116677@gmail.com>
2025-07-07 13:10:13 -0400

    Done.

--- src/test/java/SeleniumPractice/JSExecutor.java
+++ src/test/java/SeleniumPractice/JSExecutor.java
@@ -8,21 +8,29 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class JSExecutor {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException{
         //Declare instance
         WebDriver driver = new ChromeDriver();
         //Maximize window
         driver.manage().window().maximize();
         //Open browser
         driver.get("https://syntaxprojects.com/simple_context_menu.php");
-        WebElement button = driver.findElement(By.xpath("//button[text]()='Hover me!']"));
+        //Button highlight
+        WebElement button = driver.findElement(By.xpath("//button[text()='Hover me!']"));
         //JS Executor
         JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("arguments(0).style.border='3px solid red'", button);
+        js.executeScript("arguments[0].style.border='5px solid red'", button);
         //Scroll down
         js.executeScript("window.scrollBy(0,1000");
-        //scroll up
+        Thread.sleep(2000);
+        //Scroll up
         js.executeScript("window.scrollBy(0,-1000");
+
+        WebElement fileUploadTab = driver.findElement(By.xpath("//a[text()='File Upload']"));
+
+        //Click Operation
+        js.executeScript("argument[0].click();", fileUploadTab);
+
 
     }
 }
